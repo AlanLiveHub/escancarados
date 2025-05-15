935b5b0ca71b9dc40b4a48bbb42daa1a836a4860
Alan Soares <alanlive.ads@gmail.com>
2025-05-15 17:44:58 -0300

    fixes

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -9,6 +9,28 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
+    fetch('./components/navbar.html').then(response => response.text())
+        .then(data => {
+            document.getElementById('navegation_bar').innerHTML = data;
+
+            // Shrink the navbar 
+            navbarShrink();
+
+            // Shrink the navbar when page is scrolled
+            document.addEventListener('scroll', navbarShrink);
+
+            //  Activate Bootstrap scrollspy on the main nav element
+            const mainNav = document.body.querySelector('#mainNav');
+            if (mainNav) {
+                new bootstrap.ScrollSpy(document.body, {
+                    target: '#mainNav',
+                    rootMargin: '0px 0px -40%',
+                });
+            };
+
+        })
+        .catch(error => console.error('Erro ao carregar o header:', error));
+
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -23,20 +45,9 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-    // Shrink the navbar 
-    navbarShrink();
+    
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    //  Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
+   
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
